696271cc906f8f930550deb296279e2c39db336c
Cassandra Heil <Cassandraheil19@gmail.com>
2020-01-12 16:50:27 -0500

    worked on javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,98 @@
-
-var wins = document.getElementById("#wins")
-var losses = document.getElementById("#losses")
-var questionindex = document.getElementById("#questionindex")
-var userguess = document.getElementById("#userguesses")
-var answer = document.getElementById("#answer")
-
-var windwaker = ["w", "i", "n", "d", "w", "a", "k", "e", "r"]
-var breathOfTheWild = ["b", "r", "e", "a", "t", "h", "o", "f", "t", "h", "e", "w", "i", "l", "d"]
-var ocarinaOfTime = ["o", "c", "a", "r", "i", "n", "a", "o", "f", "t", "i", "m", "e"]
-var majorasMask = ["m", "a", "j", "o", "r", "a", "s", "m", "a", "s", "k"]
-var linksAwakening = ["l", "i", "n", "k", "s", "a", "w", "a", "k", "e", "n", "i", "n", "g"]
-var twilightPrincess = ["t", "w", "i", "l", "i", "g", "h", "t", "p", "r", "i", "n", "c", "e", "s", "s"]
-var skywardSword = ["s", "k", "y", "w", "a", "r", "d", "s", "w", "o", "r", "d",]
-
-var spaces = "_"
-
-var games = [windwaker, breathOfTheWild, ocarinaOfTime, majorasMask, linksAwakening, twilightPrincess, skywardSword];
-
-
-// var wordlengths =["_ _ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _ _ _ _ _"]
+window.onload = function() {
+    var windwaker = ["w", "i", "n", "d", "w", "a", "k", "e", "r"];
+    var breathOfTheWild = ["b", "r", "e", "a", "t", "h", "o", "f", "t", "h", "e", "w", "i", "l", "d"];
+    var ocarinaOfTime = ["o", "c", "a", "r", "i", "n", "a", "o", "f", "t", "i", "m", "e"];
+    var majorasMask = ["m", "a", "j", "o", "r", "a", "s", "m", "a", "s", "k"];
+    var linksAwakening = ["l", "i", "n", "k", "s", "a", "w", "a", "k", "e", "n", "i", "n", "g"];
+    var twilightPrincess = ["t", "w", "i", "l", "i", "g", "h", "t", "p", "r", "i", "n", "c", "e", "s", "s"];
+    var skywardSword = ["s", "k", "y", "w", "a", "r", "d", "s", "w", "o", "r", "d",];
 
 
 
+    var games = ["windwaker", "breathOfTheWild", "ocarinaOfTime", "majorasMask", "linksAwakening", "twilightPrincess", "skywardSword"];
+    var randomGames = games[Math.floor(Math.random()* games.length)];
+    var wordlength = document.getElementById("answer");
+    wordLength = [];
 
 
-//make the spaces, pick whats on the page, make a function excute when pg loads and when win/loss
- document = fuction(event) {
-    for (var i = 0; i <games.length; i++){
-    }
-    .textcontent = spaces * games[i].length
- }
+
+   
+   for (var i = 0; i < windwaker.length; i++){
+       wordLength[i] = "_"
+       console.log(wordLength[i]);
+   }
+
+    var answer = document.getElementById("answer");
+    answer.textContent = wordLength.join(" ");
 
 
-document.onkeyup = function(event) {
-    var guess = event.key
-    console.log(guess)
+    
+ 
 
-    for (var i = 0; i <games.length; i++){
-        console.log(games[i]) 
-        if (guess === games[i]){
-            console.log("match found")
-            alert("great guess")
-            answer.textContent = guess
+    document.onkeyup = function(event) {
+         var guess = event.key
+         console.log(guess)
 
+        var questionindex = document.getElementById("questionindex");
+        var attemptsLeft = 10;
+        attempsLeft = questionindex
+        questionindex.textContent = "attempts left: " + attemptsLeft
+                         
+            var userguess = document.getElementById("userguess") 
+            var guessed = "";
+            for (var i = 0; i < windwaker.length; i++){
 
-            //have match appear on screen
-            //that letter to appear under userguesses
-        }
-        else {
-            //letter to appear under userguesses
-            //question index down by one
+                guessed.push(guess); ///don't know why this won't work for me -.-
+             guessed = guess +guessed;
+            userguess.textContent = "already guessed: " + guess  /// cannot get it to keep guesses on page
 
-        }
-    }
+               
+            
+            
+            
+            
+                if (windwaker[i] === guess){
+                    console.log("match found")
+                    guess === wordLength[i]
+                    
 
-    //object.textContent = event.key;
+                };
+                if (guess.indexOf(windwaker) == -1){
+                    console.log("wrong");
+                    attemptsLeft -= 1; 
+                    console.log(attemptsLeft) /// need to get this to show up on page
+                    if (attemptsLeft === 0){
+                        alert("you loose")
+                    }
 
-    //     document.getElementsByName(questionindex)(questionindex + i);  }
+                }
+            }
         
 
-    userguess.textContent = "Your guess's so far ..." + guess
-    questionindex.textContent = "attempts left ..." + questionindex 
-    wins.textContent = "you got it right " + wins + " times"
-    losses.textContent = "you failed " + losses + " times" 
-}
+
+            // else {
+            //     console.log("wrong")
+            //     userguess.textContent = event.key
+                
+            //     questionindex.textContent = "attempts left ..." + questionindex + -i
+            //     console.log(questionindex)
+            // }
+            
+        }
+    }
+ 
 
 
 
+//     document.getElementsByName(questionindex)(questionindex - 1);  
+// // Function to render questions.                         to move onto the next question-translate to next array i guess
+// function renderQuestion() {
+//     // If there are still more questions, render the next one.
+//     if (questionIndex <= (questions.length - 1)) {
+//       document.querySelector("#question").innerHTML = questions[questionIndex].q;
+//     }
+//     // If there aren't, render the end game screen.
+//     else {
+//       document.querySelector("#question").innerHTML = "Game Over!";
+//       document.querySelector("#score").innerHTML = "Final Score: " + score + " out of " + questions.length;} s}
 
